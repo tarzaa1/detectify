fb06a272ecc45a8d9b51462d4576c40c0f47e23a
Tarek Zaarour <tarek.zaarour@dell.com>
2023-12-06 07:54:25 +0000

    updated default app address

--- detectify/conf.py
+++ detectify/conf.py
@@ -1,5 +1,5 @@
 from decouple import config
 
-APP_ADDRESS = config('APP_ADDRESS', default='localhost')
+APP_ADDRESS = config('APP_ADDRESS', default='0.0.0.0')
 APP_PORT = config('APP_PORT', default=5000)
 DEBUG = config('DEBUG', default=False)
