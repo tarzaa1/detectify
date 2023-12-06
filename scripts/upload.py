1c31087f9c4c06a4be72df546e3bf6215b2890ae
Tarek Zaarour <tarek.zaarour@dell.com>
2023-12-06 07:53:54 +0000

    hardcoding url and image path

--- scripts/upload.py
+++ scripts/upload.py
@@ -1,8 +1,7 @@
 import requests
-from decouple import config
 
-url = config('ENDPOINT_URL', default='http://127.0.0.1:5000/upload')
-image_path = config('IMAGE_PATH', default='data/dog_bike_car.jpg') 
+url = 'http://127.0.0.1:8000/upload'
+image_path = 'data/dog_bike_car.jpg' 
 with open(image_path, 'rb') as f:
     byte_im = f.read()
 
